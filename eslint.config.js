8e81c504ef31b6cd4fbcb871a3670bc9a862910e
zhaoguangxu <zhaoguangxu801@gmail.com>
2025-06-03 23:28:52 +0800

    feat: 更新 ESLint 配置，调整文件匹配规则，优化 Vue 组件解析器，增强代码检查的灵活性和准确性

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import eslint from '@eslint/js'
 import tseslint from '@typescript-eslint/eslint-plugin'
 import tseslintParser from '@typescript-eslint/parser'
 import vuePlugin from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 import prettierConfig from '@vue/eslint-config-prettier'
 
 export default [
@@ -10,41 +11,38 @@ export default [
   },
   eslint.configs.recommended,
   {
-    files: ['**/*.{js,ts,vue}'],
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslintParser,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: { vue: vuePlugin },
+    rules: {
+      ...vuePlugin.configs.base.rules,
+      ...vuePlugin.configs.essential.rules,
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    files: ['**/*.{js,ts}'],
     languageOptions: {
       parser: tseslintParser,
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        process: 'readonly',
+        ecmaFeatures: { jsx: true },
       },
     },
-    plugins: {
-      '@typescript-eslint': tseslint,
-      vue: vuePlugin,
-    },
-    rules: {
-      'vue/multi-word-component-names': 'off',
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-    },
-  },
-  {
-    files: ['**/*.vue'],
-    rules: {
-      ...vuePlugin.configs.base.rules,
-      ...vuePlugin.configs.essential.rules,
-    },
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
+    plugins: { '@typescript-eslint': tseslint },
     rules: {
       ...tseslint.configs.recommended.rules,
+      'no-console': 'warn',
+      'no-debugger': 'warn',
     },
   },
   prettierConfig,
